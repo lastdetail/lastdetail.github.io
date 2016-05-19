e6bb733163a743660223f2313b5a096317e525e3
DrewHiggins <me@drewhiggins.com>
2016-05-18 21:42:15 -0400

    Made more progress, just need contact form
    - Added full reviews modal making use of an iframe to keep the very
      specific old layout
    - Toggled modal buttons on mobile/desktop. Dekstop shows modals, mobile
      just links to plain text pages or hides some buttons that are too
      verbose for phone users
    - Added photos to the gallery. Currently they don't expand, but the
      images aren't very high res anyway

---
+++ mail.php
@@ -0,0 +1,35 @@
+<?php
+echo var_dump($_POST);
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+ 
+$EmailTo = "emailaddress@test.com";
+$Subject = "New Message Received";
+ 
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+ 
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+ 
+// send email
+// $success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+echo $Body . "\n";
+
+// redirect to success page
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+ 
+?>
